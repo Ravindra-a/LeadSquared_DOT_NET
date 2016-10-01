7bf9a111e61df3cfcbaa749c25a9cc35180003de
ravi_drivojoy <ravi@drivojoy.com>
2016-10-01 21:24:50 +0530

    First Commit

---
+++ LeadSquared/Helpers/LeadSquaredRestClient.cs
@@ -0,0 +1,57 @@
+﻿using LeadSquared.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeadSquared.Helpers
+{
+    /// <summary>
+    /// REstClient Wrapper to push data to LeadSquared API
+    /// </summary>
+    public class LeadSquaredRestClient
+    {
+        private readonly RestClient _client;
+        private string _apicontrollername;
+
+        private readonly string _url = ConfigurationManager.AppSettings["leadsquaredbaseurl"]; //https://api.leadsquared.com/v2/
+        private readonly string accessKey = ConfigurationManager.AppSettings["leadSquaredAccessKey"]; //<YOUR LEADSQUARED ACCESS KEY>
+        private readonly string secretKey = ConfigurationManager.AppSettings["leadsquaredSecret"]; //<YOUR LEADSQUARED SECRET>
+
+        public LeadSquaredRestClient(string apicontrollername)
+        {
+            _apicontrollername = apicontrollername;
+            _client = new RestClient(_url);
+        }
+
+        /// <summary>
+        /// Method to push data to LeadSquared
+        /// </summary>
+        /// <param name="leadJsonString"></param>
+        /// <returns></returns>
+        public Boolean AddLead(List<LeadParameter> leadJsonString)
+        {
+            try
+            {
+                var request = new RestRequest(_apicontrollername + "?accessKey=" + accessKey + "&secretKey=" + secretKey, Method.POST) { RequestFormat = DataFormat.Json };
+                request.AddBody(leadJsonString);
+                var response = _client.Execute(request);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return true;
+                }
+                //log the error and return
+                return false;
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+    }
+}
---
+++ LeadSquared/Program.cs
@@ -0,0 +1,25 @@
+﻿using LeadSquared.Helpers;
+using LeadSquared.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeadSquared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeadSquaredRestClient leadSquaredRestClient = new LeadSquaredRestClient(ConfigurationManager.AppSettings["leadsquaredcreateleadURL"]); //LeadManagement.svc/Lead.Create
+            List<LeadParameter> model = new List<LeadParameter>();
+            model.Add(new LeadParameter { Attribute = "EmailAddress", Value = "test@test.com" });
+            model.Add(new LeadParameter { Attribute = "Phone", Value = "1234567890" });
+            leadSquaredRestClient.AddLead(model);
+        }
+    }
+
+
+}
